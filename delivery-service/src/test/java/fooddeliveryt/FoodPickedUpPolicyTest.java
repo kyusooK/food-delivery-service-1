3e97bd83bcb5f9a2b0de0ab0c90a6e7c4ecc490b
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-12-21 11:32:32 +0900

    commit

--- delivery-service/src/test/java/fooddeliveryt/FoodPickedUpPolicyTest.java
+++ delivery-service/src/test/java/fooddeliveryt/FoodPickedUpPolicyTest.java
@@ -2,12 +2,16 @@ package untitled;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 ...
 public class FoodPickedUpPolicyTest {
 private static final Logger LOGGER = LoggerFactory.getLogger(EventTest.class);
-...
+
+@Test
 public void testDeliveryArranged() {
 DeliveryArranged entity = new DeliveryArranged();
-...
+// 비교 및 단언문 작성
+assertEquals(expectedValue, actualValue);
+assertTrue(statement);
 }
 }
