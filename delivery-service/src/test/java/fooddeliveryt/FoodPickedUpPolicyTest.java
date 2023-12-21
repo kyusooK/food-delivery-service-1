b2d1cd6d134cb13d788a9ec3e7c1c764837a3713
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-12-21 11:34:20 +0900

    commit

--- delivery-service/src/test/java/fooddeliveryt/FoodPickedUpPolicyTest.java
+++ delivery-service/src/test/java/fooddeliveryt/FoodPickedUpPolicyTest.java
@@ -3,15 +3,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 ...
 public class FoodPickedUpPolicyTest {
-private static final Logger LOGGER = LoggerFactory.getLogger(EventTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventTest.class);
 
-@Test
-public void testDeliveryArranged() {
-DeliveryArranged entity = new DeliveryArranged();
-// 비교 및 단언문 작성
-assertEquals(expectedValue, actualValue);
-assertTrue(statement);
-}
+    @Test
+    public void testDeliveryArranged() {
+        DeliveryArranged entity = new DeliveryArranged();
+        // 비교 및 단언문 작성
+        String expectedValue = "Expected Value";
+        String actualValue = entity.toString();
+        assertEquals(expectedValue, actualValue);
+
+        assertTrue(actualValue, entity.validate());
+    }
 }
